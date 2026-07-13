6b519b95f83a60ff1531b7377566323190280732
updinasir <updinasir@users.noreply.github.com>
2026-07-13 07:50:34 +0300

    Simplify password requirements to 8 chars + uppercase only

--- utils/security.js
+++ utils/security.js
@@ -9,15 +9,6 @@ const validatePasswordStrength = (password) => {
   if (!/[A-Z]/.test(password)) {
     errors.push('Password must contain at least one uppercase letter');
   }
-  if (!/[a-z]/.test(password)) {
-    errors.push('Password must contain at least one lowercase letter');
-  }
-  if (!/[0-9]/.test(password)) {
-    errors.push('Password must contain at least one digit');
-  }
-  if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
-    errors.push('Password must contain at least one special character');
-  }
   return {
     isValid: errors.length === 0,
     errors
